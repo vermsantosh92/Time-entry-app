20c91c1a8f818e8ae4d35314fc475b3e6cfa1137
Santosh Verma <santosh.verma.92@outlook.com>
2020-04-28 23:14:40 +0530

    setup-app

---
+++ backend/emails/account.js
@@ -0,0 +1,24 @@
+
+const sgMail = require('@sendgrid/mail');
+const sendGridAPIKey = "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";
+
+
+sgMail.setApiKey(sendGridAPIKey);
+
+
+const sendWelcomeEmail = (email, name)=>{
+    sgMail.send({
+      to : "email",
+      from : 'santosh.verma.92@outlook.com',
+      subject : "Thanks for Joining us, we're excited you're here!!",
+      text :
+
+      ` Welcome to the app, ${email}
+  we are so happy to have you! let's remove some busy work from your day
+
+      `
+    })
+}
+
+module.exports =  sendWelcomeEmail
+
